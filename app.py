d0e386d3111b14d245b1cb34dc1be60069b858c5
billmats96 <vmatsouk@auth.gr>
2020-04-17 14:03:50 +0300

    Added total tests endpoint

--- app.py
+++ app.py
@@ -4,21 +4,27 @@ from flask import make_response
 
 import json
 import copy
+import pandas as pd
 
 app = Flask(__name__)
 
 data_greece_JHCSSE = None
 data_greece_isMOOD_regions = None
+data_greece_wikipedia = None
 
 def init():
     global data_greece_JHCSSE
     global data_greece_isMOOD_regions
+    global data_greece_wikipedia
 
     with open('data/greece/JohnsHopkinsCSSE/timeseries_greece.json') as f:
         data_greece_JHCSSE = json.load(f)['Greece']
     with open('data/greece/isMOOD/regions.json') as regions_file:
     	data_greece_isMOOD_regions = json.load(regions_file)
-
+    with open('data/greece/wikipedia/cases.csv', encoding = 'utf-8') as cases_file:
+    	data_greece_wikipedia = pd.read_csv(cases_file)
+    data_greece_wikipedia = data_greece_wikipedia.where(pd.notnull(data_greece_wikipedia), None)
+    
 @app.route('/all', methods=['GET'])
 def get_all():
 
@@ -72,6 +78,15 @@ def get_active():
 
     return jsonify({'cases': out_json})
 
+@app.route('/total-tests', methods=['GET'])
+def get_total_tests():
+    
+    date = list(data_greece_wikipedia['Ημέρα'])[:-1]
+    total_tests = list(data_greece_wikipedia['Έλεγχοι δειγμάτων'])[:-1] 
+    total_tests = [int(i) if i!=None else None for i in total_tests]
+    out_json = [{'date': date, 'tests':tests} for date, tests in zip(date, total_tests)] 
+
+    return jsonify({'tests_performed_cumulative': out_json})
 
 
 @app.errorhandler(404)
